ee389e8ca3ccc40d155ef2ab3a88219b00458889
Jon-Erik <jdc151@case.edu>
2018-05-03 09:36:02 -0400

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 //Connection with local database configured
 var mysql = require("mysql");
 var password = require("./password.js");
+var connection;
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: password,
-	database: "reading_list_db"
-})
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: password,
+		database: "reading_list_db"
+	});
+}
 
 connection.connect(function(err) {
 	if (err) throw err;
